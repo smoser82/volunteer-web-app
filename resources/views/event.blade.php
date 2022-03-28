180537ac12869ebd19dae947b5131399fecd7d35
Christopher Cox <chriscox228@gmail.com>
2022-03-28 12:26:16 -0400

    Updated layout and style of the event viewing page.

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -6,6 +6,9 @@
 
         <title>Volunteering</title>
 
+        <!-- Icon stylesheet -->
+        <link rel="stylesheet" href="https://cdn.materialdesignicons.com/4.9.95/css/materialdesignicons.min.css">
+
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
@@ -17,9 +20,15 @@
     </head>
     <body class="antialiased">
         @include('header')
-        <a href="/">< Back To Event List</a>
-        <p> {{ $event->title }} </p>
-        <p> {{ $event->description }} </p>
-        <p> {{ $event->dates }} </p>
+        <a href="/" class="back-button"><span class="mdi mdi-arrow-left"></span> Back To Event List</a>
+        <h1> {{ $event->title }} </h1>
+        <hr>
+        <h3> <i class="mdi mdi-calendar-range"></i> Date and Time</h3>
+        <p>{{ $event->dates }} </p>
+        <h3> <i class="mdi mdi-text"></i> Description</h3>
+        <p>{{ $event->description }} </p>
+        <h3><i class="mdi mdi-account-box-outline"></i> Contact Information</h3>
+        <p>&ltContact Name&gt <!-- {{ $event->nameContact }} --> at &lt<a href="mailto:temp@abc.com">Contact Email</a>&gt<!-- <a href="mailto:{{ $event->emailContact }}>"{{ $event->emailContact }}</a>--></p>
+        <!-- Replace text above between "&lt" and "&gt" with the commented sections after they are implemented -->
     </body>
 </html>
