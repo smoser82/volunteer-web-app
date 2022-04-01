91aa4a82b0211faae41eb213bfcfc5875dfe87e6
Robert Eisner IV <reisneriv@gmail.com>
2022-04-01 12:38:22 -0400

    Add limit to user email field

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name_first', 25);
             $table->string('name_last', 25);
-            $table->string('email')->unique();
+            $table->string('email', 50)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->integer('account_type');
