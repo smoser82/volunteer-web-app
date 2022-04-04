f11a3f6078a1b73301529f86dc4fc68e9dff3fd0
Joshua Morford <joshua55550000@hotmail.com>
2022-04-04 17:11:57 -0400

    Update event creation page

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -24,8 +24,16 @@
             <input type="text" id="title" name="title"><br><br>
             <label for="description">Description:</label>
             <input type="text" id="description" name="description"><br><br>
-            <label for="dates">Dates:</label>
-            <input type="text" id="dates" name="dates"><br><br>
+            <label for="date_start">Date Start:</label>
+            <input type="date" id="date_start" name="date_start"><br><br>
+            <label for="date_end">Date End:</label>
+            <input type="date" id="date_end" name="date_end"><br><br>
+            <label for="visibility">Published:</label>
+            <input type="checkbox" id="visibility" name="visibility" checked><br><br>
+            <label for="contact_name">Contact Name:</label>
+            <input type="text" id="contact_name" name="contact_name"><br><br>
+            <label for="contact_email">Contact Email:</label>
+            <input type="text" id="contact_email" name="contact_email"><br><br>
             <input type="submit" value="Submit">
         </form> 
     </body>
