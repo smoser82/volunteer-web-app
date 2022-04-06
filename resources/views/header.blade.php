8c18b8cf13569b3a0cb6c3e567743a626752363d
Joshua Morford <joshua55550000@hotmail.com>
2022-04-06 14:27:53 -0400

    Remove broken links in header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -10,11 +10,7 @@
             </div>
             <div class="header_right">
                 <a href="/">Browse events</a>
-                <a href="/my_events">My events</a>
                 <a href="/create">Create event</a>
-                <a href="/login">Log in</a>
-                <a href="/signup">Sign up</a>
-                <a href="/profile">My account</a>
             </div>
         </div>
     </body>
