6f67398de4cf79f6ce5a38792f7e36719a0bc02a
Joshua Morford <joshua55550000@hotmail.com>
2022-04-23 16:04:02 -0400

    Add list of timeslots for event on event page

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -19,6 +19,37 @@
                         <p>{{ $event->description }} </p>
                         <h3><i class="mdi mdi-account-box-outline"></i> Contact Information</h3>
                         <p>{{ $event->contact_name }} at &lt<a href="mailto:{{ $event->contact_email }}">{{ $event->contact_email }}</a>&gt</p>
+
+                        @auth
+                            <br/><h1>Timeslots:</h1>
+                            @foreach ($timeslots as $timeslot)
+                                @if ($timeslot->id_user == 0)
+                                    <form method="POST" action="/signup">
+                                        {{ csrf_field() }}
+                                        <a class="event" href="#" onclick="this.parentElement.submit()">
+                                        <input type="hidden" value="{{ $timeslot->id }}" id="id_timeslot" name="id_timeslot">
+                                            <div class='card'>
+                                                <p><b>{{ $timeslot->name }}</b> </br>
+                                                Timeframe: <span>{{ $timeslot->datetime_start }}</span> to <span>{{ $timeslot->datetime_end }}</span></p>
+                                            </div>
+                                        </a>
+                                    </form>
+                                @else
+                                    <form method="POST" action="/removeSignup">
+                                        {{ csrf_field() }}
+                                        <a class="event" href="#" onclick="this.parentElement.submit()">
+                                        <input type="hidden" value="{{ $timeslot->id }}" id="id_timeslot" name="id_timeslot">
+                                            <div class='card'>
+                                                <p><b>{{ $timeslot->name }}</b> SIGNED UP!</br>
+                                                Timeframe: <span>{{ $timeslot->datetime_start }}</span> to <span>{{ $timeslot->datetime_end }}</span></p>
+                                            </div>
+                                        </a>
+                                    </form>
+                                @endif
+                            @endforeach
+                        @else
+                            <br/><h2>Please login to see available timeslots for this event.</h2>
+                        @endauth
                     </div>
                 </div>
             </div>
