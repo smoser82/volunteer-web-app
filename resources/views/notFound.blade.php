7abc02c0fcdda76c479145e51ee88d4cf908ceb5
Joshua Morford <joshua55550000@hotmail.com>
2022-04-23 10:46:33 -0400

    Fix notFound page styling

--- resources/views/notFound.blade.php
+++ resources/views/notFound.blade.php
@@ -1,25 +1,14 @@
 <x-app-layout>
     <x-slot name="header">
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Volunteering</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-        <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
-        </style>
     </x-slot>
 
     <x-slot name="slot">
-        <div class="center">
-            <img src="{{ asset('img/notFound.png') }}" alt="Image of man fishing" style="width:300px;">
+        <div class="center-text">
+            <img src="{{ asset('img/notFound.png') }}" alt="Image of man fishing" style="width:300px;" class="center-img">
             <h1> Oops...Event not found!</h1>
-            <button onclick="location.href='/'" type="button">Browse Events</button>
+            <x-button class="ml-3" onclick="location.href='/'">
+                {{ __('Browse Events') }}
+            </x-button>
         </div>
     </x-slot>
 </x-app-layout>
