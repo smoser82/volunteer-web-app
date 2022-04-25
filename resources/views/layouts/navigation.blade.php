d0a0ba4ae9cec063d4d3036bfc926a20bbf4d32f
Joshua Morford <joshua55550000@hotmail.com>
2022-04-25 17:59:06 -0400

    Make site logo link to browse page

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,7 +5,7 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="{{ route('browse') }}">
                         <img src="{{ asset('img/logo.png') }}" alt="TruLife" style="width:150px;">
                     </a>
                 </div>
